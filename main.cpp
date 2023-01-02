6f9050ad028ecc12ea4819acf0880eb28d22abdb
Riyon Aryono <riyonaryono12@gmail.com>
2023-01-02 17:19:54 +0700

    add fitur transaksi

--- main.cpp
+++ main.cpp
@@ -1,139 +1,241 @@
 #include <iostream>
+#include <string>
+#include <iomanip>
+#include <conio.h>
+#include <vector>
 
 using namespace std;
 
-string product[] ={"Album  Formula of Love", "Post Card Jihyo"};
-int harga[]   ={300000, 150000};
+vector <string> code;
+vector <string> product;
+vector <int> stock;
+vector <int> price;
 
+vector <string> cart_code;
+vector <string> cart_product;
+vector <int>    cart_qty;
+vector <int>    cart_price;
 
-// Koding Disini ya kawan kawan
+// clear sceren
 
-// pajak
-void pajak(){
-    float cost,count,total,total1,total2,tax;
-    cout << "\nHarga barang yang dibeli :";
-    cin  >> cost;
-    cout << "Jumlah barang yang dibeli :";
-    cin  >> count;
-    total1 = cost*count;
-    cout << "Total harga adalah :" <<total1<<endl;
-
-    tax = total*0.11;
-    total2 = total1 + tax;
-    cout << "\nPajak : "<<tax<<endl;
-    cout << "==========================="<<endl;
-    cout << "\nTotal pembayaran : " <<total2;
+void clrscr()
+{
+    system("cls");
 }
 
+// display_product
 
-
-// list_produk
-
-void list_produk(){
-    int m;
-    cout << "               Daftar Produk              \n";
-    cout << "==========================================\n";
-    cout << "No" << "     " <<  "Nama Produk" << "     " << "Harga" << endl;
-    cout << "==========================================\n";
-    for (m=0; m<2; m++){
-        cout << m+1 << "     " << product[m] << "     " << harga[m] << endl;
+void display_product(){
+    clrscr();
+    cout << "=========================== List Produk =============================="<<endl;
+    cout << "======================== Twice Merchandise ==========================="<<endl;
+    cout << left << setw(5) << "No" << left << setw(15 ) << "Kode Produk" << left << setw(30) << "Nama Produk" << left << setw(8) << "Stok"  << left << setw(15) << "Harga Produk" << endl;
+    cout << "======================================================================\n";
+    for (int i =0; i < product.size(); i++){
+         cout << left << setw(5)  << i + 1
+              << left << setw(15) << code[i]
+              << left << setw(30) << product[i]
+              << left << setw(8)  << stock[i]
+              << left << setw(15) << price[i]
+              << endl;
     }
-    cout << "==========================================\n";
- 
+     cout << "======================================================================\n";
  }
-
-
 
 // tambah produk
 
-void tambah_produk(){
-     string nama_barang[5], harga_barang [5];
-        int jml_barang;
-        cout << " " << endl;
-        cout << "Input Daftar Nama Barang dan Harganya : ";
+void add_product(){
+     clrscr();
+     int jml_product,harga_product,stok_product;
+   
+     cout << "========== Tambah Produk ==========="<<endl;
+     cout << "========= Twice Merchandise ========"<<endl;
+     cout << " " << endl;
+     cout << "Jumlah produk yang ingin ditambahkan : ";
+     cin >> jml_product;
+     cout << " " << endl;
 
-     cin >> jml_barang;
-     string nama,harga;
-     for ( int i = 0; i<jml_barang; i++) {
+     for ( int i = 0;  i < jml_product; i++) {
+        string nama_product, code_product;
 
-     cout << " Nama Barang " << i + 1 << " : " ;
-     cin >> nama ;
-     cout << " Harga Barang " << " : " ;
-     cin >> harga;
+        cout << "Kode produk " << i + 1 << " : ";       
+        getline(cin >> ws, code_product);
 
-     nama_barang  [i] = nama;
-     harga_barang [i] = harga;
+        cout << "Nama produk   : ";     
+        getline(cin >> ws, nama_product);
 
- }
+        cout << "Stok produk   : ";
+        cin  >> stok_product;
+
+        cout << "Harga produk  : ";
+        cin  >> harga_product;
+
+        code.push_back(code_product);
+        product.push_back(nama_product);
+        stock.push_back(stok_product);
+        price.push_back(harga_product); 
+
+        cout << " "<<endl;  
+    }
+    
+
 }
-
 
 // diskon
 
-void diskon(){
-    cout << "Diskon" << endl;
+int discount(int subtotal){
 
-    int total_belanja, harga_akhir;
+    int harga_akhir;
 
-    cout << "Total Belanja: Rp. ";
-    cin >> total_belanja;
-    cout << endl;
-
-    if ((total_belanja >= 100000) && (total_belanja < 500000)){
-        harga_akhir = total_belanja - (0.1*total_belanja);
-        cout << "Selamat, anda mendapatkan diskon 10%" << endl;
+    if ((subtotal >= 350000) && (subtotal <= 500000)){
+        harga_akhir = subtotal * 0.1;
     }
 
-    else if ((total_belanja >= 500000) && (total_belanja < 1000000)){
-        harga_akhir = total_belanja - (0.2*total_belanja);
-        cout << "Selamat, anda mendapatkan diskon 20%" << endl;
+    else if ((subtotal >= 500000) && (subtotal <= 1000000)){
+        harga_akhir = subtotal * 0.2;
     }
 
-     else if (total_belanja >= 1000000){
-        harga_akhir = total_belanja - (0.3*total_belanja);
-        cout << "Selamat, anda mendapatkan diskon 30%" << endl;
+     else if (subtotal >= 1000000){
+         harga_akhir = subtotal * 0.3;
     }
 
     else {
-        harga_akhir = total_belanja;
+        harga_akhir = 0;
     }
-    cout << "Total bayar: Rp. " << harga_akhir << endl;
-    cout << harga_akhir;
+    return harga_akhir;
+}
+
+// pajak
+int tax(int subtotal){
+    float tax;
+    tax = subtotal * 0.11;
+    return tax; 
+}
+
+// Search Product
+
+int search_product(string search, vector <string> code){
+
+     for (int i = 0; i < code.size(); ++i)
+        {
+            if (code[i] == search)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+}
+
+// receipt
+
+void receipt(int subtotal){
+    float diskon = discount(subtotal);
+    float pajak = tax(subtotal - diskon);
+    float total = (subtotal - diskon ) +  pajak;
+    long bayar, kembali = total - bayar;
+
+    cout << "\n\n=========================== Struk Belanja ============================"<<endl;
+    cout << "========================== Twice Merchandise ========================="<<endl;
+    cout << left << setw(5) << "No" << left << setw(15) << "Kode Produk" << left << setw(30) << "Nama Produk" << left << setw(8) << "Qty" << left << setw(15) << "Harga Produk" << endl;
+    
+    for (int i =0; i < cart_code.size(); i++){
+         cout << left << setw(5)  << i + 1
+              << left << setw(15) << cart_code[i]
+              << left << setw(30) << cart_product[i]
+              << left << setw(8)  << cart_qty[i]
+              << left << setw(15) << cart_price[i]
+              << endl;
+    }
+    cout << "----------------------------------------------------------------------\n";
+    cout << left << setw(45) << " " << left << setw(15) << "Sub Total : Rp." << subtotal <<endl;
+    cout << left << setw(45) << " " << left << setw(15) << "Diskon    : Rp." <<  diskon  <<endl;
+    cout << left << setw(45) << " " << left << setw(15) << "PPN 11%   : Rp." <<  pajak   <<endl;
+    cout << left << setw(45) << " " << left << setw(15) << "Total     : Rp." <<  total   <<endl;
+    cout << left << setw(45) << " " << left << setw(15) << "Bayar     : Rp.";
+    cin  >> bayar;
+    cout << left << setw(45) << " " << left << setw(15) << "Kembali   : Rp." << kembali  <<endl;
+}
+
+// Transaction
+
+void transaction(vector <string> code){
+    string search;
+    int j, index,qty, subtotal;
+
+    display_product();
+    
+    cout << "\n\n============================= Transaksi =============================="<<endl;
+    cout << "----------------------------------------------------------------------\n";
+    cout << "Jumlah Barang yang ingin dibeli :";
+    cin  >> j;
+    cout << " " << endl;
+
+    for(int i = 0; i < j; i++){
+        cout << "Masukan Kode Produk " << i + 1 << " : ";
+        cin  >> search;
+        cout << "Masukan Jumlah Barang : ";
+        cin  >> qty;
+        index = search_product(search, code);
+        
+        subtotal += qty *  price[index];
+        
+        stock[i] -= qty;
+        cart_code.push_back(code[index]);
+        cart_product.push_back(product[index]);
+        cart_qty.push_back(qty);
+        cart_price.push_back(price[index]);
+        
+        cout << " " << endl;
+    }
+
+    receipt(subtotal);
 }
 
 
 int main(){
     int menu_dashboard;
-    
-    cout << "========== Dashboard ==========="<<endl;
-    cout << "======= Twice Merchandise ======"<<endl;
-    cout << "1. Produk"<<endl;
-    cout << "2. Tambah Produk"<<endl;
-    cout << "3. Transaksi"<<endl;
-    cout << "4. Exit"<<endl;
-    cout << "Masukan Menu [1-4] : ";
-    cin  >> menu_dashboard;
+    string repeat;
 
-    switch (menu_dashboard)
+    do
     {
-    case 1:
-        cout << "Produk";
-        break;
-    case 2:
-        cout << "Tambah Produk";
-        break;
-    case 3:
-        cout << "Transaksi";
-        break;
-    case 4:
-        cout << "Selamat tinggal"<<endl;
-        return 0;
-        break;
-    default:
-        cout << "Menu yang anda inputkan salah!"<<endl;
-        break;
-    }
+        clrscr();
+        cout << "========== Dashboard ==========="<<endl;
+        cout << "======= Twice Merchandise ======"<<endl;
+        cout << "1. Produk"<<endl;
+        cout << "2. Tambah Produk"<<endl;
+        cout << "3. Transaksi"<<endl;
+        cout << "4. Exit"<<endl;
+        cout << "Masukan Menu [1-4] : ";
+        cin  >> menu_dashboard;
+        
+        switch (menu_dashboard)
+        {
+            case 1:
+                display_product();
+                break;
+            case 2:
+                add_product();
+                break;
+            case 3:
+                transaction(code);
+                break;
+            case 4:
+                cout << "Selamat tinggal"<<endl;
+                return 0;
+                break;
+            default:
+                cout << "Menu yang anda inputkan salah!"<<endl;
+                break;
+        }
 
+        cout <<endl<<endl;
+        cout << "Apakah ingin melanjutkan program? Tekan y untuk lanjut atau n untuk exit program....";
+        repeat = getch();
+        
+    } while (repeat == "Y" || repeat == "y");
+   
 
+    return 0;
 }
 
